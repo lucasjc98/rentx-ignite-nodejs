68acb7a6a742b86493698b19f97ccd3407ffa953
Lucas Costa <lucasjcosta.98@gmail.com>
2021-03-26 11:07:58 -0300

    Add user avatar

--- src/modules/accounts/usesCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/usesCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,31 @@
+import { inject, injectable } from "tsyringe";
+import { deleteFile } from "../../../../utils/File";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+@injectable()
 class UpdateUserAvatarUseCase {
-  async execute() {}
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByID(user_id);
+
+    if (user?.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
+
+    if (user) {
+      user.avatar = avatar_file;
+      await this.usersRepository.create(user);
+    }
+  }
 }
 
 export { UpdateUserAvatarUseCase };
